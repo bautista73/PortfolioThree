491e816b2e3f42133abe32ddaef86373339d754a
bbautista73 <bbautista73@gmail.com>
2023-03-02 19:00:37 -0500

    hero wip

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-// Smooth Scrolling JS
+let elements = document.querySelectorAll('.rolling-text');
 
-import Lenis from '@studio-freight/lenis'
+elements.forEach(element => {
+  let innerText = element.innerText;
+  element.innerHTML = '';
+  
+  let textContainer = document.createElement('div');
+  textContainer.classList.add('block');
+  
+  for (let letter of innerText) {
+    let span = document.createElement('span');
+    span.innerText = letter.trim() === '' ? '\xa0': letter;
+    span.classList.add('letter');
+    textContainer.appendChild(span);
+  }
+  
+  element.appendChild(textContainer);
+  element.appendChild(textContainer.cloneNode(true));
+});
 
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-  direction: 'vertical', // vertical, horizontal
-  gestureDirection: 'vertical', // vertical, horizontal, both
-  smooth: true,
-  mouseMultiplier: 1,
-  smoothTouch: false,
-  touchMultiplier: 2,
-  infinite: false,
-})
-
-lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
-  console.log({ scroll, limit, velocity, direction, progress })
-})
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
 
 
 
