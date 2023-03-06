e330e8a0b5df110986a4bbba1b4adf443fc57465
bbautista73 <bbautista73@gmail.com>
2023-03-05 20:31:18 -0500

    styled work section

--- src/index.js
+++ src/index.js
@@ -47,3 +47,37 @@ window.addEventListener("scroll", function() {
   }
 });
 
+
+// scroll animation
+
+const degreeToRadian = (angle) => {
+  return angle * (Math.PI / 180);
+};
+
+const radius = 50;
+const diameter = radius * 2;
+
+const circle = document.querySelector("#circle");
+circle.style.width = `${diameter}px`;
+circle.style.height = `${diameter}px`;
+
+const text = circle.dataset.text;
+const characters = text.split("");
+
+const deltaAngle = 360 / characters.length;
+const characterOffsetAngle = 2;
+let currentAngle = -90;
+
+characters.forEach((character, index) => {
+  const span = document.createElement("span");
+  span.innerText = character;
+  const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+  const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+
+  const transform = `translate(${xPos}px, ${yPos}px)`;
+  const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
+  span.style.transform = `${transform} ${rotate}`;
+
+  currentAngle += deltaAngle;
+  circle.appendChild(span);
+});
