958915147a0eb6c707d1786287709640807e87c0
bbautista73 <bbautista73@gmail.com>
2023-03-08 00:38:29 -0500

    finished base code, need to add mobile

--- src/index.js
+++ src/index.js
@@ -61,3 +61,26 @@ scrollLinks.forEach(link => {
     });
   });
 });
+
+const nav = document.getElementById("nav");
+
+for(const link of nav.getElementsByTagName("a")) {  
+  link.onmousemove = e => {
+    const rect = link.getBoundingClientRect(),    
+          img = link.querySelector("img");
+    
+    img.style.left = `${e.clientX - rect.left}px`;
+    img.style.top = `${e.clientY - rect.top}px`;
+  }
+}
+
+// window.onmousemove = e => {
+//   const percent = e.clientY / window.innerHeight;
+  
+//   nav.animate({
+//     transform: `translateY(${percent * nav.offsetHeight * -0.07}px)`
+//   }, {
+//     fill: "forwards",
+//     duration: 1000
+//   })
+// }
