fbdb6d4fc85e20b6856cca54f2c7545f38486022
bbautista73 <bbautista73@gmail.com>
2023-03-09 16:58:02 -0500

    fixing works section

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
-// works hover effect
+// works hover and expand
 
 const nav = document.getElementById("nav");
 
-for(const link of nav.getElementsByTagName("a")) {  
-  link.onmousemove = e => {
-    const rect = link.getBoundingClientRect(),    
-          img = link.querySelector("img");
-    
-    img.style.left = `${e.clientX - rect.left}px`;
-    img.style.top = `${e.clientY - rect.top}px`;
+class Project {
+  constructor(title, description, technologies) {
+    this.title = title;
+    this.description = description;
+    this.technologies = technologies;
   }
 }
+
+for (const link of nav.getElementsByTagName("a")) {
+  link.onmousemove = e => {
+    const rect = link.getBoundingClientRect(),
+      img = link.querySelector("img");
+
+    img.style.left = `${e.clientX - rect.left}px`;
+    img.style.top = `${e.clientY - rect.top}px`;
+  };
+
+  link.onclick = e => {
+    e.preventDefault(); // prevent link behavior 
+  
+    const details = link.querySelector(".project-details");
+    const img = link.querySelector("img");
+
+    if (details.style.maxHeight) {
+      details.style.maxHeight = null;
+      img.style.opacity = 1;
+    } else {
+      details.style.maxHeight = `${details.scrollHeight}px`;
+      img.style.opacity = 0;
+    }
+  };
+  
+}
+
+
+
+
+
+// const nav = document.getElementById("nav");
+
+// for(const link of nav.getElementsByTagName("a")) {  
+//   link.onmousemove = e => {
+//     const rect = link.getBoundingClientRect(),    
+//           img = link.querySelector("img");
+    
+//     img.style.left = `${e.clientX - rect.left}px`;
+//     img.style.top = `${e.clientY - rect.top}px`;
+//   }
+// }
+
+// projects expand 
