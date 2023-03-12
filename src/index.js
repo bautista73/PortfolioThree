de999b6b9eaa46c29368530537f61961bf80eee5
bbautista73 <bbautista73@gmail.com>
2023-03-12 16:53:48 -0400

    working works section again, restyle

--- src/index.js
+++ src/index.js
@@ -1,40 +1,56 @@
 // works hover and expand
 
-let linkClicked = false;
+const titles = document.querySelectorAll('.titles-list li');
+const projectImages = document.querySelectorAll('.project-image');
 
-for (const link of nav.getElementsByTagName("a")) {
-  link.onmousemove = e => {
-    const rect = link.getBoundingClientRect(),
-      img = link.querySelector("img");
+titles.forEach((title, index) => {
+  title.addEventListener('mouseover', () => {
+    const image = title.getAttribute('data-image');
+    projectImages[index].style.backgroundImage = `url(${image})`;
+  });
+  title.addEventListener('mouseout', () => {
+    projectImages[index].style.backgroundImage = '';
+  });
+});
 
-    if (!linkClicked) {
-      img.style.opacity = 1; // only set opacity to 1 if link hasn't been clicked
-    }
-    img.style.left = `${e.clientX - rect.left}px`;
-    img.style.top = `${e.clientY - rect.top}px`;
-  };
 
-  link.onclick = e => {
-    e.preventDefault(); // prevent link behavior 
+
+
+// let linkClicked = false;
+
+// for (const link of nav.getElementsByTagName("a")) {
+//   link.onmousemove = e => {
+//     const rect = link.getBoundingClientRect(),
+//       img = link.querySelector("img");
+
+//     if (!linkClicked) {
+//       img.style.opacity = 1; // only set opacity to 1 if link hasn't been clicked
+//     }
+//     img.style.left = `${e.clientX - rect.left}px`;
+//     img.style.top = `${e.clientY - rect.top}px`;
+//   };
+
+//   link.onclick = e => {
+//     e.preventDefault(); // prevent link behavior 
   
-    const details = link.querySelector(".project-details");
-    const img = link.querySelector("img");
+//     const details = link.querySelector(".project-details");
+//     const img = link.querySelector("img");
 
-    if (details.style.maxHeight) {
-      details.style.maxHeight = null;
-      img.style.opacity = 1; // set image opacity back to 1
-      linkClicked = false; // reset flag
-    } else {
-      details.style.maxHeight = `${details.scrollHeight}px`;
-      img.style.opacity = 0;
-      linkClicked = true; // set flag
-    } 
-  };
+//     if (details.style.maxHeight) {
+//       details.style.maxHeight = null;
+//       img.style.opacity = 1; // set image opacity back to 1
+//       linkClicked = false; // reset flag
+//     } else {
+//       details.style.maxHeight = `${details.scrollHeight}px`;
+//       img.style.opacity = 0;
+//       linkClicked = true; // set flag
+//     } 
+//   };
 
-  link.onmouseleave = e => {
-    const img = link.querySelector("img");
-    if (!linkClicked) {
-      img.style.opacity = 0; // only set opacity to 0 if link hasn't been clicked
-    }
-  };
-}
+//   link.onmouseleave = e => {
+//     const img = link.querySelector("img");
+//     if (!linkClicked) {
+//       img.style.opacity = 0; // only set opacity to 0 if link hasn't been clicked
+//     }
+//   };
+// }
