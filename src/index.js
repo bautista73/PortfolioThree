b2395ee8952a939f2a64fd34ceeb366bb085de42
bbautista73 <bbautista73@gmail.com>
2023-03-13 01:30:11 -0400

    fixed works section and finsihed styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// works hover and expand
+// works hover
 
 const titles = document.querySelectorAll('.titles-list li');
 const projectImages = document.querySelectorAll('.project-image');
@@ -7,50 +7,43 @@ titles.forEach((title, index) => {
   title.addEventListener('mouseover', () => {
     const image = title.getAttribute('data-image');
     projectImages[index].style.backgroundImage = `url(${image})`;
+    projectImages[index].classList.add('visible');
   });
   title.addEventListener('mouseout', () => {
     projectImages[index].style.backgroundImage = '';
+    projectImages[index].classList.remove('visible');
   });
 });
 
+// works modal 
 
+const popupWindow = document.querySelector('#popup-window');
+const popupContent = document.querySelector('#popup-content');
+const closeButton = document.querySelector('#close-button');
+const popupTriggers = document.querySelectorAll('.popup-trigger');
 
+function displayPopup(content) {
+  popupContent.innerHTML = content.innerHTML;
+  popupWindow.style.display = 'block';
+  popupWindow.classList.add('fade-in-bottom');
+}
 
-// let linkClicked = false;
-
-// for (const link of nav.getElementsByTagName("a")) {
-//   link.onmousemove = e => {
-//     const rect = link.getBoundingClientRect(),
-//       img = link.querySelector("img");
-
-//     if (!linkClicked) {
-//       img.style.opacity = 1; // only set opacity to 1 if link hasn't been clicked
-//     }
-//     img.style.left = `${e.clientX - rect.left}px`;
-//     img.style.top = `${e.clientY - rect.top}px`;
-//   };
-
-//   link.onclick = e => {
-//     e.preventDefault(); // prevent link behavior 
+popupTriggers.forEach(popupTrigger => {
+  const popupContentId = popupTrigger.getAttribute('data-popup-content');
+  const popupContent = document.querySelector(popupContentId);
   
-//     const details = link.querySelector(".project-details");
-//     const img = link.querySelector("img");
+  popupTrigger.addEventListener('click', () => {
+      displayPopup(popupContent);
+  });
+});
 
-//     if (details.style.maxHeight) {
-//       details.style.maxHeight = null;
-//       img.style.opacity = 1; // set image opacity back to 1
-//       linkClicked = false; // reset flag
-//     } else {
-//       details.style.maxHeight = `${details.scrollHeight}px`;
-//       img.style.opacity = 0;
-//       linkClicked = true; // set flag
-//     } 
-//   };
+function closePopup() {
+  popupWindow.classList.add('fade-out-top');
+    setTimeout(() => {
+        popupWindow.style.display = 'none';
+        popupWindow.classList.remove('fade-out-top');
+    }, 500);
+}
 
-//   link.onmouseleave = e => {
-//     const img = link.querySelector("img");
-//     if (!linkClicked) {
-//       img.style.opacity = 0; // only set opacity to 0 if link hasn't been clicked
-//     }
-//   };
-// }
+closeButton.addEventListener('click', closePopup);
+
