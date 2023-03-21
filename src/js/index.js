ec4f1745aa547e1eb111eeaa3cb705700e83e5ba
bbautista73 <bbautista73@gmail.com>
2023-03-21 15:45:33 -0400

    edited transitions with gsap for a fluid animation between tabs

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import { Scene } from '../js/script.js';
 
 const tabs = document.querySelectorAll('.tab');
 const tabContents = document.querySelectorAll('.tab-content');
+const webglShader = document.querySelector('.webgl-shader');
 
 const scene = new Scene();
 
@@ -16,9 +17,9 @@ tabs.forEach((tab) => {
 
     tabContents.forEach((tabContent) => {
       if (tabContent.id === target) {
-        tabContent.classList.add('active');
+        tabContent.classList.add('visible');
       } else {
-        tabContent.classList.remove('active');
+        tabContent.classList.remove('visible');
       }
     });
 
@@ -26,40 +27,77 @@ tabs.forEach((tab) => {
 
     let newPosition;
 
+    
     switch (target) {
+      
       case 'tab1':
-        newPosition = { x: 0, y: 0, z: 6 };
+        newPosition = { x: 0, y: 0, z: 7 };
+        tabContents.forEach((tabContent) => {
+          if (tabContent.id === target) {
+            gsap.fromTo(
+              tabContent, 
+              {opacity: 0}, 
+              { duration: 1, opacity: 1, ease: 'power3.in', delay: 0.6 });
+          } else {
+            gsap.to(
+              tabContent, 
+              {duration: 0.5, opacity: 0,});
+          }
+        });
         break;
+
       case 'tab2':
-        newPosition = { x: 0, y: 0, z: 0 };
+        newPosition = { x: 0, y: 0, z: 2 };
+        tabContents.forEach((tabContent) => {
+          if (tabContent.id === target) {
+            gsap.fromTo(
+              tabContent, 
+              { opacity: 0,}, 
+              { duration: 1, opacity: 1, ease: 'power3.in', delay: 0.6 });
+          } else {
+            gsap.to(
+              tabContent, 
+              { duration: 0.5, opacity: 0,});
+          }
+        });
         break;
+
       case 'tab3':
         newPosition = { x: 0, y: 0, z: 3 };
+        tabContents.forEach((tabContent) => {
+          if (tabContent.id === target) {
+            gsap.fromTo(
+              tabContent, 
+              { opacity: 0,}, 
+              { duration: 1, opacity: 1, ease: 'power3.in', delay: 0.6 });
+          } else {
+            gsap.to(
+              tabContent, 
+              { duration: 0.5, 
+                opacity: 0,});
+          }
+        });
         break;
+
       default:
         newPosition = { x: 0, y: 0, z: 6 };
     }
 
     gsap.to(scene.camera.position, {
-      duration: 1, // Set duration in seconds
-      x: newPosition.x,
-      y: newPosition.y,
-      z: newPosition.z,
-      ease: 'power2.inOut', // Set easing function
+        duration: 1,
+        x: newPosition.x,
+        y: newPosition.y,
+        z: newPosition.z,
+        ease: 'power2.inOut',
     });
   });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const defaultTab = document.querySelector('.tab[data-default]');
+  if (defaultTab) {
+    defaultTab.click();
+  }
+})
 
-
-
-
-// const canvasButton = document.querySelector('#canvas-button-square');
-// const canvas = document.querySelector('#webgl');
-
-// canvasButton.addEventListener('click', () => {
-//   canvas.style.width = '50%';
-//   canvas.style.right = '0';
-//   canvas.style.transition = 'width 0.5s ease-in-out, right 0.5s ease-in-out';
-// });
 
